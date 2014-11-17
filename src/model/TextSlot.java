8f8127cb70a40bc1e8c7c937bdd7e00c4279b253
Vilhelm Lundqvist <dat13vlu@alfa-15.student.lth.se>
2014-11-17 14:58:18 +0100

    La till en fråga i TextSlot

--- src/model/TextSlot.java
+++ src/model/TextSlot.java
@@ -5,6 +5,7 @@ import expr.Environment;
 public class TextSlot implements Slot {
 	private String txt;
 	
+	//TODO ska man lägga till en '#' när man kallar toString eller ska den finnas med i txt-String:en hela tiden?
 	public TextSlot(String txt) {
 		this.txt = txt;
 	}
