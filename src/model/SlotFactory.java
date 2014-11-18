3cf1c4d4ec08d043e4cdc94f82167bafb5913d2b
pigmato <pigmato@gmail.com>
2014-11-18 11:23:40 +0100

    factory

--- src/model/SlotFactory.java
+++ src/model/SlotFactory.java
@@ -1,9 +1,33 @@
 package model;
 
+import java.io.IOException;
+
+import expr.ExprParser;
+
 public class SlotFactory {
+	private String contents;
+	StringBuilder sb;
 
 	public Slot buildSlot(String input) {
-		//TODO
-		return null;
+		sb = new StringBuilder(input);
+		char letter = sb.charAt(0);
+		if (letter == '#') {
+			sb.deleteCharAt(0);
+			contents = sb.toString();
+			return new TextSlot(contents);
+
+		} else {
+			ExprParser parser = new ExprParser();
+			
+			try {
+				return new ExprSlot(parser.build(input));
+			} catch (IOException e) {
+				throw new XLException(e.getMessage());
+			}
+			
+	
+
+		}
+
 	}
 }
