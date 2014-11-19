d467d6b819a83ebd80524e11cac54bd19c0c2119
Vilhelm <v.lundqvist94@gmail.com>
2014-11-19 12:28:01 +0100

    Added the usage of CurrentAddress into the SlotLabel class. Added a CurrentAddress parameter to the constructor of SlotLabel, and recursively to each class that then needed a CurrentAddress parameter in their constructors

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -9,8 +9,9 @@ import java.util.Observer;
 public class SlotLabel extends ColoredLabel implements ActionListener, Observer {
 	private char col;
 	private int row;
+	private CurrentAddress curr;
 	
-    public SlotLabel(char col, int row) {
+    public SlotLabel(char col, int row, CurrentAddress curr) {
         super("                    ", Color.WHITE, RIGHT);
         this.col = col;
         this.row = row;
@@ -19,6 +20,7 @@ public class SlotLabel extends ColoredLabel implements ActionListener, Observer 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		//TODO kalla på att currentLabel i mainsheet ska uppdateras
+		curr.setCurrent(col + "" + row);
 	}
 
 	@Override
