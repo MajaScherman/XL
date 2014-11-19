c78dc3340d21a88159f8f360128439ec80f44fc0
KristianBerg <kristianberg.jobb@gmail.com>
2014-11-19 14:35:48 +0100

    Massive rewrite of SlotLabel and SlotLabel

--- src/gui/CurrentAddress.java
+++ src/gui/CurrentAddress.java
@@ -1,21 +1,33 @@
 package gui;
 
+import java.awt.Color;
 import java.util.Observable;
 
 public class CurrentAddress extends Observable{
-	private String current;
+	private SlotLabel current;
 	
 	public CurrentAddress(){
-		current = "A1";
+		current = new SlotLabel('A', 1, this);
 	}
 	
-	public void setCurrent(String newCurrent){
+	public void setCurrent(SlotLabel newCurrent){
+		current.setBackground(Color.WHITE);
 		current = newCurrent;
+		current.setBackground(Color.YELLOW);
 		setChanged();
 		notifyObservers();
+		System.out.println(current.toString());
 	}
 	
 	public String getCurrent(){
-		return current;
+		return current.toString();
+	}
+	
+	public char getCol(){
+		return current.getCol();
+	}
+	
+	public int getRow(){
+		return current.getRow();
 	}
 }
