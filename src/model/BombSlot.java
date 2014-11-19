560639d2a59ded786fff9ffbbebb7de3cf04c235
KristianBerg <kristianberg.jobb@gmail.com>
2014-11-19 12:56:05 +0100

    Created and implemented BombSlot class for detecting circular dependencies in expressions

---
+++ src/model/BombSlot.java
@@ -0,0 +1,19 @@
+package model;
+
+import expr.Environment;
+
+public class BombSlot implements Slot {
+
+	@Override
+	public double value(Environment env) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString(Environment env) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
